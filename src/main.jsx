8207ed4549866a5470b0b1c3dc95ae107de44cd3
Wellington Brito <71724775+wellington86333@users.noreply.github.com>
2026-04-14 13:52:52 -0300

    feat: reforça confiança com transparência e política LGPD

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { LanguageProvider } from './LanguageContext.jsx'
+import PrivacyPolicyPage from './components/PrivacyPolicyPage.jsx'
+
+const path = window.location.pathname;
+const isPrivacyPage = path === '/politica-de-privacidade';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <LanguageProvider>
-            <App />
+            {isPrivacyPage ? <PrivacyPolicyPage /> : <App />}
         </LanguageProvider>
     </React.StrictMode>,
 )
